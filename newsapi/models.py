9f0a72c8272a60e5aeedc258bce733d3ddb2035f
Rquaicoo <russellquaicoo1@gmail.com>
2023-02-13 10:36:36 +0000

    migrate models to database

--- newsapi/models.py
+++ newsapi/models.py
@@ -1,17 +1,19 @@
 from django.db import models
+import datetime
 
 # Create your models here.
-class NewsO(models.Model):
+class News(models.Model):
     title = models.CharField(max_length=2000)
     description = models.TextField()
     url = models.URLField()
-    image = models.URLField()
-    publishedAt = models.DateTimeField()
-    content = models.TextField()
-    source = models.CharField(max_length=100)
-    category = models.CharField(max_length=100)
-    country = models.CharField(max_length=100)
-    language = models.CharField(max_length=100)
+    image = models.URLField(default="")
+    publishedAt = models.DateTimeField(default=datetime.datetime.now)
+    content = models.TextField(default="")
+    source = models.CharField(max_length=100, default="")
+    category = models.CharField(max_length=100, default="general")
+    country = models.CharField(max_length=100, default="worldwide")
+    language = models.CharField(max_length=100, default="en")
+    date_scraped = models.DateTimeField(auto_now_add=True)
     author = models.CharField(max_length=100)
     
     def __str__(self):
--- newsapi/views.py
+++ newsapi/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 # Create your views here.
+class NewsAPIView():
+    authentication_classes  = [AllowAny]
+
+    def get(self, request):
+        """
+        Parameters:
+
+        category (str)
+        country (str)
+        language (str)
+        """
+        category = request.GET.get("category")
+        country = request.GET.get("country")
+        language = request.GET.get("language")
+
+        if not category:
+            category = "general"
+        if not country:
+            country = "worldwide"
+        if not language:
+            language = "en"
