1a5c326ff44b7858d32c1c6ca5da679666290886
Rquaicoo <russellquaicoo1@gmail.com>
2023-02-13 10:37:33 +0000

    isolate firebase authentication class

--- accounts/views.py
+++ accounts/views.py
@@ -11,22 +11,6 @@ from rest_framework.permissions import AllowAny
 import os
 
 
-credentials = credentials.Certificate({
-    "type": "service_account",
-    "project_id": os.environ.get("project_id"),
-    "private_key_id": os.environ.get("private_key_id"),
-    "private_key": os.environ.get("private_key"),
-    "client_email": os.environ.get("client_email"),
-    "client_id": os.environ.get("client_id"),
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-zp0a4%40news-app-66795.iam.gserviceaccount.com"
-})
-
-default_app = firebase_admin.initialize_app(credentials)
-
-
 def authenticateWithFirebase(firebase_id_token):
     try:
         #verify firebase id token
@@ -37,6 +21,7 @@ def authenticateWithFirebase(firebase_id_token):
         user_email = ""
 
         try:
+            print("User found: ", auth.get_user(user_id))
             user = auth.get_user(user_id).email
             user_email = user.email
 
@@ -47,19 +32,6 @@ def authenticateWithFirebase(firebase_id_token):
 
     return user_id, user_email, provider
 
-
-class FirebaseAuthentication(authentication.BaseAuthentication):
-    def authenticate(self, request):
-        
-        token = request.headers.get('Authorization')
-        if not token:
-            return None
-
-        try:
-            decoded_token = auth.verify_id_token(token)
-            user_id = decoded_token['uid']
-        except:
-            return None
 
 # Create your views here.
 class CreateUserAPIView(APIView):
@@ -74,6 +46,9 @@ class CreateUserAPIView(APIView):
         firebase_id_token = request.data["firebase_id_token"]
         firstname = request.data["name"].split(" ")[0]
         lastname = request.data["name"].split(" ")[1]
+
+        if User.objects.filter(username=user_id).exists():
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
 
         try:
             user_id, email, _ = authenticateWithFirebase(firebase_id_token)
