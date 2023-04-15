6edb3c6ca770bf9ffed5862a9e6eab2ae11cfd94
Rquaicoo <russellquaicoo1@gmail.com>
2023-04-15 17:17:36 +0000

    add models for authors

--- influxnews/newsapi/models.py
+++ influxnews/newsapi/models.py
@@ -9,12 +9,19 @@ class News(models.Model):
     image = models.URLField(default="")
     publishedAt = models.DateTimeField(default=datetime.datetime.now)
     content = models.TextField(default="")
-    source = models.CharField(max_length=100, default="")
     category = models.CharField(max_length=100, default="general")
     country = models.CharField(max_length=100, default="worldwide")
     language = models.CharField(max_length=100, default="en")
     date_scraped = models.DateTimeField(auto_now_add=True)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, default=1, null=True)
     
     def __str__(self):
         return self.title
+    
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.URLField(default="")
+    
+    def __str__(self):
+        return self.name
--- influxnews/newsapi/serializers.py
+++ influxnews/newsapi/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import News
+from .models import News, Author
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ['id', 'name', 'image']
 
 class NewsSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
     class Meta:
         model = News
-        fields = ['id', 'title', 'description', 'url', 'urlToImage', 'publishedAt', 'content', 'source', 'category', 'country', 'language', 'date_scrapped', 'author']
+        fields = ['id', 'title', 'description', 'url', 'publishedAt', 'content', 'category', 'country', 'language', 'date_scraped', 'author']
