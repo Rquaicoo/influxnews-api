9efe07190b7eba67f1dd7164ab1ef46c44053448
Rquaicoo <russellquaicoo1@gmail.com>
2023-05-05 01:45:50 +0000

    slight database field modifcations

--- influxnews/newsapi/models.py
+++ influxnews/newsapi/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class News(models.Model):
-    title = models.CharField(max_length=2000)
+    title = models.CharField(max_length=9000)
     description = models.TextField()
     url = models.URLField()
     image = models.URLField(default="")
@@ -14,6 +16,11 @@ class News(models.Model):
     language = models.CharField(max_length=100, default="en")
     date_scraped = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, default=1, null=True)
+
+    viewed_by = models.ManyToManyField(User, related_name="viewed_news", blank=True)
+    saved_by = models.ManyToManyField(User, related_name="saved_news", blank=True)
+
+    view_count = models.IntegerField(default=0)
     
     def __str__(self):
         return self.title
@@ -25,3 +32,20 @@ class Author(models.Model):
     
     def __str__(self):
         return self.name
+    
+class Recommendation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username + " " + self.news.title
+
+class UserActivity(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    action = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.user.username + " " + self.action + " " + self.news.title
