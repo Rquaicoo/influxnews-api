56d7d34feb2bc9af78e5977291c746fc8bc72cb3
Rquaicoo <russellquaicoo1@gmail.com>
2023-06-11 01:16:34 +0000

    fetch news by category

--- influxnews/newsapi/views.py
+++ influxnews/newsapi/views.py
@@ -22,7 +22,7 @@ class NewsAPIView(APIView):
 
     authentication_classes  = [] #change this to firebase authentication
 
-    def get(self, request, language="en", country="worldwide", category="general", latest=False, query=None):
+    def get(self, request):
         """
         Parameters:
 
@@ -31,18 +31,30 @@ class NewsAPIView(APIView):
         category (str)
         latest (bool) 
         """
+
+        language = request.GET.get('language', 'en')
+        country = request.GET.get('country', 'worldwide')
+        category = request.GET.get('category')
+        latest = request.GET.get('latest', False)
+        query = request.GET.get('query')
+
         if query:
 
-            all_news_items = News.objects.filter(language=language, country=country, category=category)
+            all_news_items = News.objects.filter(language=language, country=country)
 
             search_results = all_news_items.order_by('-publishedAt').filter(title__icontains=query)
 
-            searialized_news = NewsSerializer(news, many=True)
+            searialized_news = NewsSerializer(search_results, many=True)
 
             return Response(searialized_news.data, status=status.HTTP_200_OK)
 
         if latest:
             news = News.objects.filter(language=language, country=country, category=category).order_by('-publishedAt')[:10]
+            searialized_news = NewsSerializer(news, many=True)
+            return Response(searialized_news.data, status=status.HTTP_200_OK)
+        
+        if category:
+            news = News.objects.filter(language=language, country=country, category=category)
             searialized_news = NewsSerializer(news, many=True)
             return Response(searialized_news.data, status=status.HTTP_200_OK)
 
@@ -61,24 +73,3 @@ class NewsScraperAPIView(APIView):
         getTechCruchHeadlines()
         return Response("News Scrapped", status=status.HTTP_200_OK)
     
-
-class CorrectUrlsAPIView(APIView):
-
-    def get(self, request):
-        #get news from bbc and bbc sport
-        bbc_news = Author.objects.get(name='BBC News')
-        bbc_sport = Author.objects.get(name='BBC Sport')
-        news = News.objects.filter(author=bbc_news)
-        sports = News.objects.filter(author=bbc_sport)
-
-        for n in news:
-            if n.url.split("//")[1][0:3] != "bbc" or n.url.split("//")[1][0:3] != 'www':
-                n.url = n.url.split("//")[0] + "//bbc.com/" + n.url.split("//")[1]
-                n.save()
-
-        for n in sports:
-            if n.url.split("//")[1][0:3] != "bbc" or n.url.split("//")[1][0:3] != 'www':
-                n.url = n.url.split("//")[0] + "//bbc.com/" + n.url.split("//")[1]
-                n.save()
-
-        return Response("Urls Corrected", status=status.HTTP_200_OK)
