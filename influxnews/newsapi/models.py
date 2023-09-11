19fecc03474809683dfbe570fb8f45566a12a602
Rquaicoo <russellquaicoo1@gmail.com>
2023-09-11 21:29:26 +0000

    track liked posts for users

--- influxnews/newsapi/models.py
+++ influxnews/newsapi/models.py
@@ -3,7 +3,14 @@ import datetime
 from django.contrib.auth.models import User
 
 
-# Create your models here.
+class UserInterests(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    interests = models.CharField(max_length=1000, default="")
+    
+    def __str__(self):
+        return self.user.username + " " + self.interests
+    
+
 class News(models.Model):
     title = models.CharField(max_length=9000)
     description = models.TextField()
@@ -23,8 +30,7 @@ class News(models.Model):
     view_count = models.IntegerField(default=0)
     
     def __str__(self):
-        return self.title
-    
+        return self.title    
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -49,3 +55,11 @@ class UserActivity(models.Model):
     
     def __str__(self):
         return self.user.username + " " + self.action + " " + self.news.title
+    
+class LikedPosts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    news = models.ForeignKey(News, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.user.username + " " + self.news.title
